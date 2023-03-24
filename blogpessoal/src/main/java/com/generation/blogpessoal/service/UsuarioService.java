2159ed1e678cd5859eb97afc0b0fa8a07933e530
Andreza Silvestre <deza.silvestre@hotmail.com>
2023-03-24 20:16:44 -0300

    atividade security atualizada

--- blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -1,4 +1,4 @@
-package com.generation.blogpessoal.security;
+package com.generation.blogpessoal.service;
 
 import java.nio.charset.Charset;
 import java.util.Optional;
@@ -7,14 +7,16 @@ import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Usuario;
 import com.generation.blogpessoal.model.UsuarioLogin;
 import com.generation.blogpessoal.repository.UsuarioRepository;
 
+@Service
 public class UsuarioService {
-
+	
 	 @Autowired
 	    private UsuarioRepository usuarioRepository;
 
@@ -95,4 +97,5 @@ public class UsuarioService {
 	        return "Basic " + new String(tokenBase64);
 
 	    }
+
 }
